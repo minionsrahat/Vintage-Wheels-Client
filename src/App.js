e88bdbd8afa83c3d9fbaf9f5d721a9bc19ff87fa
minionsrahat <rahatuddin786@gmail.com>
2022-05-23 01:47:57 +0600

    Project init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import { Route, Routes } from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+      <Routes>
+      
+      </Routes>
+    </>
+  );
+}
+
+export default App;
