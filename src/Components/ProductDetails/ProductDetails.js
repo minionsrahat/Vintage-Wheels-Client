e6555d4b8e4f61efba18a51019844121302fe66d
minionsrahat <rahatuddin786@gmail.com>
2022-05-24 15:24:45 +0600

    place order functionality added

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -1,25 +1,99 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 import { useNavigate, useParams } from 'react-router-dom';
 import Spinner from '../Spinner/Spinner';
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Rating from 'react-rating';
+import auth from '../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 
 const ProductDetails = () => {
 
+	const [user,loading]=useAuthState(auth)
 	const { id } = useParams();
 	const navigate = useNavigate();
+	const [userData,setUserdata]=useState({})
+	const arrivalDate = new Date(new Date().getTime()+(5*24*60*60*1000));
+	
+
+	useEffect(()=>{
+        fetch(`http://localhost:5000/readUserData?email=${user?.email}`)
+		.then(res=>res.json())
+		.then(data=>setUserdata(data))
+	},[user])
+
+
 	const { isLoading: productLoading, data: product } = useQuery('productdata', () =>
 		fetch(`http://localhost:5000/readSingleToolsData/${id}`).then(res =>
 			res.json()
 		)
 	)
+	const phone = useRef('');
+    const quantity = useRef('');
+    const address = useRef('');
+    const message = useRef('');
+	
 	if (productLoading) {
 		return <Spinner></Spinner>
 	}
 
+	const handleQuantity=(e)=>{
+		if(parseInt(quantity.current.value)<product.min_quantity)
+		{
+			quantity.current.value=product.min_quantity
+			alert('You have to order at least '+product.min_quantity+" items")
+		}
+	   else	if(isNaN(quantity.current.value))
+		{
+			quantity.current.value=product.min_quantity
+			alert('Quantity can not be string')
+		}
+		else if(parseInt(quantity.current.value)>product.quantity)
+		{
+			
+			alert('Sorry We havent '+quantity.current.value+" items in our stock")
+			quantity.current.value=product.min_quantity
+		}
+	   
+	}
 
+
+	const handleFormsubmit = (e) => {
+        const token = localStorage.getItem('accessToken')
+        const newOrder = {
+			product_id:product._id,
+            phone: phone.current.value,
+            quantity: quantity.current.value,
+            address: address.current.value,
+            message: message.current.value,
+            email:userData.email,
+			name:userData.name
+        }
+        e.preventDefault();
+        fetch("http://localhost:5000/addOrder", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                accesstoken: `${userData.email} ${token}`
+            },
+            body: JSON.stringify(newOrder)
+        }).then(res => res.json())
+            .then((data) => {
+                const { acknowledged, insertedId } = data;
+                const { error } = data
+
+                if (acknowledged) {
+                    alert('Order Added Successfully')
+                }
+                else {
+                    alert("Unexpected Error Occured!! Please Fill Up form carefully")
+                }
+            }) 
+    }
+	
+	
 
 	return (
 		<>
@@ -89,41 +163,41 @@ const ProductDetails = () => {
 						</div>
 						<div class="col-md-6">
 							<div class="wow fadeInUp" data-wow-delay="0.2s">
-								<form>
+								<form onSubmit={handleFormsubmit}>
 									<div class="row g-3">
 										<div class="col-md-6">
 											<div class="form-floating">
-												<input type="text" class="form-control" id="name" placeholder="Your Name" />
+												<input type="text" class="form-control"readOnly id="name" placeholder={"Your Name"} value={userData?.name} />
 												<label for="name">Your Name</label>
 											</div>
 										</div>
 										<div class="col-md-6">
 											<div class="form-floating">
-												<input type="email" class="form-control" id="email" placeholder="Your Email" />
+												<input type="email"readOnly class="form-control" id="email" value={userData?.email} placeholder="Your Email" />
 												<label for="email">Your Email</label>
 											</div>
 										</div>
 										<div class="col-12">
 											<div class="form-floating">
-												<input type="text" class="form-control" id="subject" placeholder="Phone" />
+												<input ref={phone} type="text" class="form-control"required id="subject" placeholder="Phone" />
 												<label for="subject">Phone</label>
 											</div>
 										</div>
 										<div class="col-12">
 											<div class="form-floating">
-												<input type="text" class="form-control" id="subject" placeholder="Address" />
+											<input ref={address} type="text" class="form-control" id="subject" placeholder="Address" />
 												<label for="subject">Address</label>
 											</div>
 										</div>
 										<div class="col-12">
 											<div class="form-floating">
-												<input type="text" class="form-control" id="subject" placeholder="Quantity" />
+												<input type="text" ref={quantity} onBlur={handleQuantity}  class="form-control" id="subject" placeholder="Quantity" />
 												<label for="subject">Quantity</label>
 											</div>
 										</div>
 										<div class="col-12">
 											<div class="form-floating">
-												<textarea class="form-control" placeholder="Leave a message here" id="message"></textarea>
+												<textarea ref={message} class="form-control" placeholder="Leave a message here" id="message"></textarea>
 												<label for="message">Message</label>
 											</div>
 										</div>
